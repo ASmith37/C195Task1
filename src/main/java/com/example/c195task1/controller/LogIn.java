27cba5f8eb38243efafb13d8bae1eff81999e1ae
AS <asmith37@gmail.com>
2022-10-06 21:26:23 -0400

    Cumulative changes

--- src/main/java/com/example/c195task1/controller/LogIn.java
+++ src/main/java/com/example/c195task1/controller/LogIn.java
@@ -1,16 +1,15 @@
 package com.example.c195task1.controller;
 
 import com.example.c195task1.helper.UserData;
+import com.example.c195task1.model.Appointment;
+import com.example.c195task1.model.AppointmentDAO;
 import com.example.c195task1.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import com.example.c195task1.model.UserDAO;
 import javafx.stage.Stage;
 
@@ -18,6 +17,7 @@ import java.io.Console;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class LogIn implements Initializable {
@@ -37,10 +37,28 @@ public class LogIn implements Initializable {
         lblLogInError.setVisible(false);
         lblLogInErrorMsg.setVisible(false);
     }
-
+    private void showAlert(String title, String message) {
+        Alert error = new Alert(Alert.AlertType.INFORMATION);
+        error.setTitle(title);
+        error.setHeaderText(title);
+        error.setContentText(message);
+        error.showAndWait();
+    }
     public void onBtnLogIn(ActionEvent actionEvent) throws SQLException, IOException {
         User user = UserDAO.checkCredentials(txtUsername.getText(), txtPassword.getText());
         if (user != null) {
+
+            Appointment appointment = AppointmentDAO.checkForAppointmentSoon();
+            if (appointment != null) {
+                showAlert("Upcoming appointment", String.format("There is an upcoming appointment.\nAppointment ID: %d\nDate: %s\nTime: %s",
+                        appointment.getAppointmentId(),
+                        appointment.getStart().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) ,
+                        appointment.getStart().format(DateTimeFormatter.ofPattern("h:mm a"))));
+            }
+            else {
+                showAlert("No upcoming appointments", "There are no appointments starting soon");
+            }
+
             UserData.username = user.getUserName();
             UserData.userId = user.getUserId();
             // load next screen
--- src/main/java/com/example/c195task1/model/AppointmentDAO.java
+++ src/main/java/com/example/c195task1/model/AppointmentDAO.java
@@ -137,4 +137,42 @@ public class AppointmentDAO {
         statement.executeUpdate(sql);
         DBConnection.closeConnection();
     }
+    public static Appointment checkForAppointmentSoon() throws SQLException {
+        DBConnection.openConnection();
+        PreparedStatement ps = DBConnection.connection.prepareStatement(
+                "SELECT Appointment_ID, `Title`, `Description`, Location, `Type`, `Start`, `End`, Create_Date, Created_By, Last_Update, Last_Updated_By, Customer_ID, User_ID, Contact_ID, Contact_Name\n" +
+                        "FROM appointments\n" +
+                        "LEFT JOIN contacts\n" +
+                        "USING (Contact_ID)\n" +
+                        "WHERE Start >= ?\n" +
+                        "AND Start <= date_add(?, interval 15 minute);"
+        );
+        ps.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+        ps.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
+        ResultSet r = ps.executeQuery();
+        if (r.next()) {
+            Appointment appointment = new Appointment(
+                    r.getInt(1),
+                    r.getString(2),
+                    r.getString(3),
+                    r.getString(4),
+                    r.getString(5),
+                    r.getTimestamp(6).toLocalDateTime(),
+                    r.getTimestamp(7).toLocalDateTime(),
+                    r.getTimestamp(8).toLocalDateTime(),
+                    r.getString(9),
+                    r.getTimestamp(10).toLocalDateTime(),
+                    r.getString(11),
+                    r.getInt(12),
+                    r.getInt(13),
+                    r.getInt(14),
+                    r.getString(15));
+            DBConnection.closeConnection();
+            return appointment;
+        }
+        else {
+            DBConnection.closeConnection();
+            return null;
+        }
+    }
 }
